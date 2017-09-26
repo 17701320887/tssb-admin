ed5b82d44ca8cfdfb6862aef394dca3c8be34c3d
zhangbobo <zhangbobo@duia.com>
2017-09-26 12:26:15 +0800

    删除配置文件

--- src/main/java/com/tssb/common/util/YxConfig.java
+++
@@ -1,8 +0,0 @@
-package com.tssb.common.util;
-
-/**
- * Created by admin on 2016/6/6.
- */
-public class YxConfig {
-    public static String appKey = JProperties.getProperties(JProperties.loadProperties("yxConfig"), "app_key");
-}
--- src/main/java/com/tssb/service/system/spi/IRoleService.java
+++
@@ -1,7 +0,0 @@
-package com.tssb.service.system.spi;
-
-import com.tssb.model.system.Role;
-
-public interface IRoleService extends IBaseService<Role,Long> {
-    String findById(Long id);
-}
